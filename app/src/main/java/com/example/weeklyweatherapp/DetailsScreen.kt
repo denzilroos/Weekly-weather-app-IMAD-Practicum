c0cd4ff16239339c6410e164dc34397cc920f935
RC_Student_lab <st10457298@rcconnect.edu.za>
2024-06-10 11:23:12 +0000

    weather app screens layouts

--- app/src/main/java/com/example/weeklyweatherapp/DetailsScreen.kt
+++ app/src/main/java/com/example/weeklyweatherapp/DetailsScreen.kt
@@ -1,11 +1,22 @@
 package com.example.weeklyweatherapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+
+lateinit var backbutton : Button
 
 class DetailsScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_details_screen)
+
+        backbutton = findViewById(R.id.backbttn)
+
+        backbutton.setOnClickListener() {
+            val intent = Intent(this, MainScreen::class.java)
+            startActivity(intent)
+        }
     }
 }
--- app/src/main/java/com/example/weeklyweatherapp/MainScreen.kt
+++ app/src/main/java/com/example/weeklyweatherapp/MainScreen.kt
@@ -1,11 +1,23 @@
 package com.example.weeklyweatherapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
+lateinit var submitbutton : Button
+lateinit var detailScreen : Button
 class MainScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_screen)
+
+        submitbutton = findViewById(R.id.submitbttn)
+        detailScreen = findViewById(R.id.viewDetailsBttn)
+
+        detailScreen.setOnClickListener() {
+            val intent = Intent(this, DetailsScreen::class.java)
+            startActivity(intent)
+        }
     }
 }
